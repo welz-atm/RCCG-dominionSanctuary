964ed83ad9351dc20f8012095e3d4195dc182bf5
Styne001 <macstine@gmail.com>
2021-08-03 15:55:04 +0100

    Update settings.py

--- dominion/settings.py
+++ dominion/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication',
     'activity',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -152,8 +153,15 @@ STATICFILES_DIRS = (
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-central-1.wasabisys.com' % AWS_STORAGE_BUCKET_NAME
+AWS_LOCATION = 'media'
+
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_ROOT = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
 LOGIN_URL = reverse_lazy('login')

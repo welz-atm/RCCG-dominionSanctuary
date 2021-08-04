61fdd9f8669ed5a0c9dc54fbd932c8b35efd83a1
Styne001 <macstine@gmail.com>
2021-08-04 18:33:39 +0100

    Update models.py

--- activity/models.py
+++ activity/models.py
@@ -17,14 +17,14 @@ class Service(models.Model):
     name = models.CharField(max_length=255, choices=SERVICE_TYPES)
     announcement = models.TextField(max_length=255, default='Bible Study')
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    video = models.FileField()
+    video = models.FileField(upload_to='media/')
 
     def __str__(self):
         return self.name
 
 
 class Photo(models.Model):
-    image = models.FileField()
+    image = models.FileField(upload_to='media/')
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
 
 

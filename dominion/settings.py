4baed9af10f79a7a3aea74e4b8436e5f69b54e50
Styne001 <macstine@gmail.com>
2021-08-19 16:21:16 +0100

    Update settings.py

--- dominion/settings.py
+++ dominion/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
